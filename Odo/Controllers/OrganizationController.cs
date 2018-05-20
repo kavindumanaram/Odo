f5939c2d0a3ae21a7cc7dff56ac4994b5fee6add
Kavindu Manaram <kavindu@calcey.com>
2018-05-20 11:20:05 +0530

    add partial view called save to org

--- Odo/Controllers/OrganizationController.cs
+++ Odo/Controllers/OrganizationController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Odo.Models;
 
 namespace Odo.Controllers
 {
-    public class OrganizationController : Controller
-    {
-        // GET: Organization
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class OrganizationController : Controller
+	{
+		// GET: Organization
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Save(int id)
+		{
+			using (OdoEntities oe = new OdoEntities())
+			{
+				var organization = oe.Organizations.Where(a => a.OrganizationId == id).FirstOrDefault();
+
+				return View(organization);
+			}
+		}
+	}
 }
