3b0f098443053f5d2e56510832459ebacc393aed
Kavindu Manaram <kavindu@calcey.com>
2018-05-22 09:59:34 +0530

    create org index page and getOrganization funtionalities

--- Odo/Controllers/OrganizationController.cs
+++ Odo/Controllers/OrganizationController.cs
@@ -7,52 +7,65 @@ using Odo.Models;
 
 namespace Odo.Controllers
 {
-    public class OrganizationController : Controller
-    {
-        // GET: Organization
-        public ActionResult Index()
-        {
-            return View();
-        }
+	public class OrganizationController : Controller
+	{
+		// GET: Organization
+		public ActionResult Index()
+		{
+			return View();
+		}
 
 		[HttpGet]
-	    public ActionResult Save(int id)
-	    {
-		    using (OdoEntities oe = new OdoEntities())
-		    {
-			    var organizationObject = oe.Organizations.Where(org => org.OrganizationId == id).FirstOrDefault();
-			    return View(organizationObject);
-		    }
-	    }
+		public ActionResult Save(int id)
+		{
+			using (OdoEntities oe = new OdoEntities())
+			{
+				var organizationObject = oe.Organizations.Where(org => org.OrganizationId == id).FirstOrDefault();
+				return View(organizationObject);
+			}
+		}
 
-	    [HttpPost]
-	    public ActionResult Save(Organization emp)
-	    {
-		    bool status = false;
-		    if (ModelState.IsValid)
-		    {
-			    using (OdoEntities dc = new OdoEntities())
-			    {
-				    if (emp.OrganizationId > 0)
-				    {
-					    //Edit 
-					    var v = dc.Organizations.Where(a => a.OrganizationId == emp.OrganizationId).FirstOrDefault();
-					    if (v != null)
-					    {
-						    v.Name = emp.Name;
-						    v.Address = emp.Address;
-					    }
-				    }
-				    else
-				    {
-					    //Save
-					    dc.Organizations.Add(emp);
-				    }
-				    dc.SaveChanges();
-				    status = true;
-			    }
-		    }
-		    return new JsonResult { Data = new { status = status } };
-	    }
+		[HttpGet]
+		public ActionResult GetOrganizations()
+		{
+			List<Organization> organizationList;
+			using (OdoEntities oe = new OdoEntities())
+			{
+				 organizationList = oe.Organizations.OrderBy(a => a.OrganizationId).ToList();
+			}
+			//	return Json(new {Data = organizationList}, JsonRequestBehavior.AllowGet);
+			return Json(organizationList, JsonRequestBehavior.AllowGet);
+		}
+
+
+		[HttpPost]
+		public ActionResult Save(Organization emp)
+		{
+			bool status = false;
+			if (ModelState.IsValid)
+			{
+				using (OdoEntities dc = new OdoEntities())
+				{
+					if (emp.OrganizationId > 0)
+					{
+						//Edit 
+						var v = dc.Organizations.Where(a => a.OrganizationId == emp.OrganizationId).FirstOrDefault();
+						if (v != null)
+						{
+							v.Name = emp.Name;
+							v.Address = emp.Address;
+						}
+					}
+					else
+					{
+						//Save
+						dc.Organizations.Add(emp);
+					}
+					dc.SaveChanges();
+					status = true;
+				}
+			}
+			return new JsonResult { Data = new { status = status } };
+		}
 	}
 }
